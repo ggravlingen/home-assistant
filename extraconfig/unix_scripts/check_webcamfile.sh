6ef93e9ef135c7d0ba33ffc9d30c88e7fe3c863f
Patrik <slasktratt@gammelgravlingen.com>
2016-12-26 20:00:19 +0000

    A

--- extraconfig/unix_scripts/check_webcamfile.sh
+++ extraconfig/unix_scripts/check_webcamfile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if find /home/hass/.homeassistant/extraconfig/webcam/. -maxdepth 1 -mmin +5 -type f | grep no1 &> /dev/null && echo success || echo fail
+if find /home/homeassistant/.homeassistant/extraconfig/webcam/. -maxdepth 1 -mmin +5 -type f | grep no1 &> /dev/null && echo success || echo fail
 then
-	sudo cp /home/hass/.homeassistant/extraconfig/webcam/offline.png /home/hass/.homeassistant/extraconfig/webcam/no1.jpg 
+	sudo cp /home/homeassistant/.homeassistant/extraconfig/webcam/offline.png /home/homeassistant/.homeassistant/extraconfig/webcam/no1.jpg 
 fi
