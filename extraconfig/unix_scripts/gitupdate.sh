15472e3df52acb2a787e37f28c4b3e9682651a59
Patrik <slasktratt@gammelgravlingen.com>
2017-02-12 10:49:25 +0100

    Minor update

--- extraconfig/unix_scripts/gitupdate.sh
+++ extraconfig/unix_scripts/gitupdate.sh
@@ -4,21 +4,11 @@
 #source /srv/homeassistant/homeassistant_venv/bin/activate
 #hass --script check_config
 
-#default="Minor update"
-#echo -n "Enter the Description for the Change: " [Minor Update]
-#read CHANGE_MSG
-
 cd /home/homeassistant/.homeassistant
 git add .
 git status
 
-read -e -p "Enter the Description for the Change:" -i "Minor" CHANGE_MSG
-
-echo $CHANGE_MSG
-
-#read -p "Enter the Description for the Change: " CHANGE_MSG
-#name=${CHANGE_MSG:-Minor}
-
+read -e -p "Enter the Description for the change: " -i "Minor update" CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
 git push origin master
 
