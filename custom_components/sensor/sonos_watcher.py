e12cd0acdf5ab4c82616f4617460f4a02650f73f
Patrik <slasktratt@gammelgravlingen.com>
2017-03-19 21:06:47 +0100

    Minor update

--- custom_components/sensor/sonos_watcher.py
+++ custom_components/sensor/sonos_watcher.py
@@ -1,113 +1,75 @@
 """ Just a different way of displaying current state of Sonos players  """
-
-# https://github.com/Amir974/home-assistant-custom-components/blob/master/custom_components/notify/pushoverglances.py
-
 import logging
 import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
-import json, sys, urllib
-
-#from homeassistant.const import (CONF_MONITORED_CONDITIONS, CONF_NAME)
+from soco import SoCo
 
 DEPENDENCIES = []
-
-CONF_START_IMAGE = 'startimage'
-CONF_PLAY = 'playimage'
-CONF_PAUSE = 'pauseimage'
-
-
-DEFAULT_IMAGE = "http://demandware.edgesuite.net/sits_pod40/dw/image/v2/ABCG_PRD/on/demandware.static/-/Sites-sonos-master/default/dw00cb461e/images/connect/connect-front.png?sw=40&sh=40"
-DEFAULT_PLAY = "https://image.flaticon.com/icons/svg/0/375.svg"
-DEFAULT_PAUSE = "https://image.flaticon.com/icons/svg/61/61039.svg"
-
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_START_IMAGE, default=DEFAULT_IMAGE): cv.string,
-    vol.Optional(CONF_PLAY, default=DEFAULT_PLAY): cv.string,
-    vol.Optional(CONF_PAUSE, default=DEFAULT_PAUSE): cv.string,
-})
 
 # Setup the logger platform
 _LOGGER = logging.getLogger(__name__)
 
-
+#SoCoData = SoCo(IP)
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setup the sensor platform."""
-    picture = config.get(CONF_START_IMAGE)
-#    name = config.get(DEFAULT_NAME)
-#    devs = []
-#    devs.append(SonosWatcher(name, force_update))
-#    add_devices(devs)
-    add_devices([ SonosWatcher(picture) ])
+  """Setup the sensor platform."""
 
+#  SoCoData = SoCo(IP)
+
+  add_devices( [ SonosWatcher() ] )
 
 class SonosWatcher(Entity):
-    """Representation of a Sensor."""
+  """Representation of a Sensor."""
 
-    def __init__(self, picture):
-        """Initialize the sensor."""
-        _LOGGER.info("Sonos Watcher: initializing")
-        self._state = None
-        self._picture = picture
-        self._myCurrentState = "Not Initialized"
+  def __init__(self):
+    _LOGGER.info("Sonos Data: initializing")
 
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return 'Sonos Vardagsrum (new)'
+    ### Debug variables
+    IP = "192.168.0.85"
 
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
+    ### Get data from the SoCo Library
+    loadSocoData = SoCo(IP)
 
-    @property
-    def entity_picture(self):
-        """Type of player."""
-        return self._picture
+    self._SoCoData = loadSocoData
+    self._model_name = loadSocoData.get_speaker_info()['model_name']
+    self._player_name = loadSocoData.player_name
 
-    @classmethod
-    def setPlayerState(self):
-        """Get current state of player
-        """
-        self._myCurrentState = "Playing"
-        return _myCurrentState
-#        try:
-#            return PushoverGlanceService(config[CONF_USER_KEY],
-#                                               config[CONF_API_KEY])
-#        except InitError:
-#            _LOGGER.error('Wrong API key supplied pushover glances. Get it at https://pushover.net')
-#            return None
-#        self._myCurrentState = "Playing"
+    ### Misc
+    self._icon = None
+    self._state = loadSocoData.get_current_transport_info()['current_transport_state']
 
-    @classmethod
-    def playerType(self):
-      """ 
-      http://192.168.0.85:1400/status/zp
-      """
+  @property
+  def name(self):
+    """Return the name of the sensor."""
+    returnName = self._player_name + ' (mini)'
+    return returnName
+
+  @property
+  def state(self):
+    """Return the state of the sensor."""
+    return self._state
+
+  @property
+  def entity_picture(self):
+    """Type of player."""
+
+    if self._model_name == "Sonos CONNECT":
+      model_image = "http://demandware.edgesuite.net/sits_pod40/dw/image/v2/ABCG_PRD/on/demandware.static/-/Sites-sonos-master/default/dw00cb461e/images/connect/connect-front.png?sw=40&sh=40"
+
+    return model_image
 
 
-    def update(self):
-        """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
-#        url_state = "http://192.168.0.140:5005/kitchen/state"
-#        response_state = urllib.urlopen(url_playlist)
-#        foo = json.load(response_state)["playbackState"]
-#        try 1 == 1:
-        self._state = "Playing"
- #       except:
-  #        _LOGGER.error('Threw error')
-   #       return None
-#          self._state = "Threw error"
-
-
-#        this.setPlayerState(self)
-
-#        currentState = getPlayerState(self)
-        #currentState = "aa" #getPlayerState()
-#        _LOGGER.info("Current state = %s", self._myCurrentState )
+  def update(self):
+    """Fetch new state data for the sensor.
+    This is the only method that should fetch new data for Home Assistant.
+    """
+    SoCoData = self._SoCoData
+    internalState = SoCoData.get_current_transport_info()['current_transport_state']
+    if internalState == "PLAYING":
+      self._state = "mdi:self.model_state_icon"
+#      self._icon = "mdi:self.model_state_icon"
+    else:
+      self._state = "mdi:pause-octagon-outline"
+#      self._icon = "mdi:pause-octagon-outline"
