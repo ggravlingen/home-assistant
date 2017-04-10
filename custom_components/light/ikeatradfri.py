0a44f9868e850faf60faa266717b7359b963cc23
Patrik <slasktratt@gammelgravlingen.com>
2017-04-10 22:44:16 +0200

    Minor update

--- custom_components/light/ikeatradfri.py
+++ custom_components/light/ikeatradfri.py
@@ -34,19 +34,19 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the IKEA Tradfri Light platform."""
 
     import openikeatradfri
-    
+
     # Assign configuration variables.
     # The configuration check takes care they are present.
     host = config.get(CONF_HOST)
     securitycode = config.get(CONF_API_KEY)
-    
+
     api = openikeatradfri.api_factory(host, securitycode)
     hub = openikeatradfri.Hub(api)
     _LOGGER.debug("IKEA Tradfri Hub | init | Initialization Process Complete")
 
     lights = hub.get_lights()
     add_devices(IKEATradfri(light) for light in lights)
-    
+
     _LOGGER.debug("IKEA Tradfri Hub | get_lights | All Lights Loaded")
 
 
@@ -57,7 +57,7 @@ class IKEATradfri(Light):
         """Initialize an IKEA Tradfri Light."""
         self._light = light
         self._name = light.name
-        self._state = None
+        self._state = None #light.lights.is_on
         self._brightness = None
 
     @property
@@ -97,99 +97,3 @@ class IKEATradfri(Light):
         self._light.update()
         self._state = self._light.is_on()
         self._brightness = self._light.brightness
-        
-
-class IKEATradfriHelper(object):
-    """ Gets information on a specific device """
-
-    def __init__(self, host, securityCode, light_id):
-        self._devices = {}
-        
-        self._light_id = str(light_id)
-
-        self._host = host
-        self._security_code = securityCode
-
-        self._name = None
-        self._state = True
-        self._brightness = None
-
-    @property
-    def name(self):
-        """ Get the name of a device  """
-        output = command_helper_v2(self._host, self._security_code, 'get', self._light_id)
-
-        try:
-            self._name = output['9001']
-        except ValueError:
-            _LOGGER.debug("IKEA Tradfri Helper | Name | Error Setting Name")
-
-        return self._name
-
-    @property
-    def brightness(self):
-        """ Get the brightness level """
-        output = command_helper_v2(self._host, self._security_code, 'get', self._light_id)
-        _LOGGER.debug( output['3311'][0]['5851'] )
-        _LOGGER.debug( output['3311']['0']['5851'] )
-        _LOGGER.debug(int(output['3311'][0]['5851']))
-
-        try:
-            self._brightness = int(output['3311'][0]['5851'])+1 # The bulbs run from 0=off to 254=max but ha is 1-255
-        except KeyError:
-            _LOGGER.debug("IKEA Tradfri Helper | Brightness | KeyError")
-        except TypeError:
-            _LOGGER.debug("IKEA Tradfri Helper | Brightness | TypeError")
-
-        return self._brightness
-
-    @property
-    def is_on(self):
-        """Return true if light is on."""
-
-        if self._brightness > 1:
-            self._state = True
-        else:
-            self._state = False
-
-        return self._state
-
-    def update(self):
-        """ Updates the state of the lamp """
-        self._state = this.is_on
-        self._brightness = this.brightness
-
-def command_helper_v2(host, security_code, method, light_id = ''):
-    """ Execute the command through shell """
-
-    #_LOGGER.debug(host + ' | ' + security_code + ' | ' + method + ' | ' + light_id)
-
-    if len(light_id) > 0:
-        commandString = 'coaps://' + host + ':5684/15001/' + light_id
-    else:
-        commandString = 'coaps://' + host + ':5684/15001'
-
-    theCommand = [
-        '/usr/local/bin/coap-client',
-        '-u',
-        'Client_identity',
-        '-k',
-        security_code,
-        '-v',
-        '0',
-        '-m',
-        method,
-        commandString
-        ]
-
-    try:
-        return_value = subprocess.check_output(theCommand)
-        out = return_value.strip().decode('utf-8')
-    except subprocess.CalledProcessError:
-        _LOGGER.debug('Command failed: %s', theCommand)
-
-    """ Return only the last line, where there's JSON """
-    output = json.loads(out.split('\n')[-1])
-
-    return output
-
