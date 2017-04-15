e1799fe154e757792e081ae6c6fef2b4fdfc22aa
Patrik <slasktratt@gammelgravlingen.com>
2017-04-15 23:18:07 +0200

    Minor update

--- custom_components/light/tradfri.py
+++ custom_components/light/tradfri.py
@@ -5,18 +5,15 @@ import logging
 
 import voluptuous as vol
 
-import homeassistant.util as util
-from homeassistant.util.color import (
-    color_temperature_mired_to_kelvin as mired_to_kelvin,
-    color_temperature_kelvin_to_mired as kelvin_to_mired)
-from homeassistant.components.light import ATTR_BRIGHTNESS, \
-    ATTR_WHITE_VALUE, SUPPORT_WHITE_VALUE, SUPPORT_BRIGHTNESS, \
-    Light, PLATFORM_SCHEMA
-from homeassistant.const import CONF_HOST, CONF_API_KEY
+from homeassistant.util.color import rgb_hex_to_rgb_list
+from homeassistant.components.light import (
+    ATTR_BRIGHTNESS, ATTR_RGB_COLOR, Light,
+    PLATFORM_SCHEMA, SUPPORT_BRIGHTNESS, SUPPORT_RGB_COLOR)
+from homeassistant.const import CONF_API_KEY, CONF_HOST
 import homeassistant.helpers.config_validation as cv
 
 
-SUPPORTED_FEATURES = (SUPPORT_BRIGHTNESS | SUPPORT_WHITE_VALUE)
+SUPPORTED_FEATURES = (SUPPORT_BRIGHTNESS | SUPPORT_RGB_COLOR)
 
 # Home Assistant depends on 3rd party packages for API specific code.
 REQUIREMENTS = ['pytradfri==0.4']
@@ -35,7 +32,6 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     import pytradfri
 
     # Assign configuration variables.
-    # The configuration check takes care they are present.
     host = config.get(CONF_HOST)
     securitycode = config.get(CONF_API_KEY)
 
@@ -62,8 +58,9 @@ class IKEATradfri(Light):
         self._light_control = light.light_control
         self._light_data = light.light_control.lights[0]
         self._name = light.name
+
         self._brightness = None
-        self._white = None
+        self._rgb_color = None
 
     @property
     def supported_features(self):
@@ -86,26 +83,34 @@ class IKEATradfri(Light):
         return self._light_data.dimmer
 
     @property
-    def white_value(self):
-        """Return the white property."""
-        return self._white
+    def rgb_color(self):
+        """RGB color of the light."""
+        return self._rgb_color
 
     def turn_off(self, **kwargs):
         """Instruct the light to turn off."""
         return self._light_control.set_state(False)
 
     def turn_on(self, **kwargs):
-        """Instruct the light to turn on."""
+        """
+        Instruct the light to turn on.
+
+        After adding "self._light_data.hexcolor is not None"
+        for ATTR_RGB_COLOR, this also supports Philips Hue bulbs.
+        """
         if ATTR_BRIGHTNESS in kwargs:
             self._light.light_control.set_dimmer(kwargs.get(ATTR_BRIGHTNESS))
+        if ATTR_RGB_COLOR in kwargs and self._light_data.hex_color is not None:
+            hex_color = '{0:02x}{1:02x}{2:02x}'.format(*kwargs[ATTR_RGB_COLOR])
+            self._light.light_control.set_hex_color(hex_color)
         else:
             self._light.light_control.set_state(True)
 
     def update(self):
-        """Fetch new state data for this light.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
+        """Fetch new state data for this light."""
         self._light.update()
         self._brightness = self._light_data.dimmer
-        #self._xy_color = self._light_data.xy_color
+
+        # Handle Hue lights paired with the gatway
+        if self._light_data.hex_color is not None:
+            self._rgb_color = rgb_hex_to_rgb_list(self._light_data.hex_color)
--- extraconfig/python_code/test_pytradfri.py
+++ extraconfig/python_code/test_pytradfri.py
@@ -5,6 +5,7 @@
 
 import sys
 import pytradfri
+from homeassistant.util.color import *
 
 # Assign configuration variables. The configuration check takes care they are present.
 api = pytradfri.coap_cli.api_factory(sys.argv[1], sys.argv[2])
@@ -18,26 +19,34 @@ print(lights)
 # Lights can be accessed by its index, so lights[1] is the second light
 
 # Example 1: checks state of the light 2 (true=on)
-print(lights[1].light_control.lights[0].state)
+print(lights[2].light_control.lights[0].state)
 
 # Example 2: get dimmer level of light 2
-print(lights[1].light_control.lights[0].dimmer)
+print(lights[2].light_control.lights[0].dimmer)
 
 # Example 3: What is the name of light 2
-print(lights[1].name)
+print(lights[2].name)
 
 # Example 4: Set the light level of light 2
-lights[1].light_control.set_dimmer(20)
+lights[2].light_control.set_dimmer(20)
 
 # Example 5: Change color of light 2
-lights[1].light_control.set_hex_color('f5faf6') # f5faf6 = cold | f1e0b5 = normal | efd275 = warm
+#lights[1].light_control.set_hex_color('f5faf6') # f5faf6 = cold | f1e0b5 = normal | efd275 = warm
+#print(lights[2].light_control.lights[0].hex_color) # f5faf6 = cold | f1e0b5 = normal | efd275 = warm
+
+moods = gateway.get_moods()
+print(moods)
+
 
 #rgb(245,250,246)
 #rgb(241,224,181)
 #rgb(239,210,117)
 
 # Example 6: Color temp
-print("Color")
-print(lights[1].light_control.lights[0].raw)
-print(lights[1].light_control.lights[0].xy_color[0])
+#print("Color")
+#print(lights[1].light_control.lights[0].raw)
+#foo = color_xy_brightness_to_RGB(lights[1].light_control.lights[0].xy_color[0], lights[1].light_control.lights[0].xy_color[1], lights[1].light_control.lights[0].dimmer)
+#print(foo)
+
+#print(lights[1].light_control.lights[0].xy_color)
 
