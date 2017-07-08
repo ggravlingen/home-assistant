eb3b4d4cc26af30122ef84ea4d0f6bc35199c914
Patrik <slasktratt@gammelgravlingen.com>
2017-07-08 21:14:29 +0200

    Minor update

---
+++ extraconfig/unix_scripts/update_floorplan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/homeassistant/.homeassistant/www/custom_ui/floorplan
+
+rm floorplan.css
+rm ha-floorplan.html
+rm svg-pan-zoom.min.js
+
+wget https://raw.githubusercontent.com/pkozul/ha-floorplan/master/www/custom_ui/floorplan/floorplan.css
+wget https://raw.githubusercontent.com/pkozul/ha-floorplan/master/www/custom_ui/floorplan/ha-floorplan.html
+wget https://raw.githubusercontent.com/pkozul/ha-floorplan/master/www/custom_ui/floorplan/svg-pan-zoom.min.js
+
