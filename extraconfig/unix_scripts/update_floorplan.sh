a33161601b971578e89718029a0f1ba764ba77bf
Patrik <slasktratt@gammelgravlingen.com>
2017-07-10 19:46:40 +0200

    Minor update

--- extraconfig/unix_scripts/update_floorplan.sh
+++ extraconfig/unix_scripts/update_floorplan.sh
@@ -2,11 +2,13 @@
 
 cd /home/homeassistant/.homeassistant/www/custom_ui/floorplan
 
+# Disabled because I have some custom css of my own
 rm floorplan.css
-rm ha-floorplan.html
-rm svg-pan-zoom.min.js
-
 wget https://raw.githubusercontent.com/pkozul/ha-floorplan/master/www/custom_ui/floorplan/floorplan.css
+
+rm ha-floorplan.html
 wget https://raw.githubusercontent.com/pkozul/ha-floorplan/master/www/custom_ui/floorplan/ha-floorplan.html
+
+rm svg-pan-zoom.min.js
 wget https://raw.githubusercontent.com/pkozul/ha-floorplan/master/www/custom_ui/floorplan/svg-pan-zoom.min.js
 
